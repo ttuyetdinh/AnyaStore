2637d88c83969c19c1718227a0b12a6d7500aea7
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-02-21 15:08:29 +0700

    AnyaStore.Web: add logger for BaseService

--- AnyaStore.Web/Services/BaseService.cs
+++ AnyaStore.Web/Services/BaseService.cs
@@ -16,10 +16,13 @@ namespace AnyaStore.Web.Services
         public IApiMessageRequestBuilder _apiMessageRequestBuilder { get; set; }
         public string BaseUrl { get; private set; }
 
-        public BaseService(IHttpClientFactory httpClientFactory, IApiMessageRequestBuilder apiMessageRequestBuilder, IConfiguration configuration)
+        private readonly ILogger<BaseService> _logger;
+
+        public BaseService(IHttpClientFactory httpClientFactory, IApiMessageRequestBuilder apiMessageRequestBuilder, IConfiguration configuration, ILogger<BaseService> logger)
         {
             _httpClientFactory = httpClientFactory;
             _apiMessageRequestBuilder = apiMessageRequestBuilder;
+            _logger = logger;
         }
         public void SetBaseUrl(string baseUrl)
         {
@@ -45,30 +48,38 @@ namespace AnyaStore.Web.Services
                     {
                         case HttpStatusCode.NotFound:
                             finalResponseDTO.ErrorMessage = new List<string>() { "Not found" };
+                            _logger.LogError($"{requestDTO.ApiType} {requestDTO.Url}", finalResponseDTO.ErrorMessage);
                             break;
                         case HttpStatusCode.Forbidden:
                             finalResponseDTO.ErrorMessage = new List<string>() { "Access denied" };
+                            _logger.LogError($"{requestDTO.ApiType}, {requestDTO.Url}: {finalResponseDTO.ErrorMessage}");
                             break;
                         case HttpStatusCode.Unauthorized:
                             finalResponseDTO.ErrorMessage = new List<string>() { "Unauthorized" };
+                            _logger.LogError($"{requestDTO.ApiType}, {requestDTO.Url}: {finalResponseDTO.ErrorMessage}");
                             break;
                         case HttpStatusCode.InternalServerError:
                             finalResponseDTO.ErrorMessage = new List<string>() { "Internal server error" };
+                            _logger.LogError($"{requestDTO.ApiType}, {requestDTO.Url}: {finalResponseDTO.ErrorMessage}");
                             break;
                         case HttpStatusCode.BadRequest:
                             var error = await response.Content.ReadAsStringAsync();
                             finalResponseDTO.ErrorMessage = new List<string> { JsonConvert.DeserializeObject<ResponseDTO>(error).ToString() };
+                            _logger.LogError($"{requestDTO.ApiType}, {requestDTO.Url}: {finalResponseDTO.ErrorMessage}");
                             break;
                         default:
                             var apiContent = await response.Content.ReadAsStringAsync();
                             finalResponseDTO = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
                             finalResponseDTO.IsSuccess = true;
+                            _logger.LogInformation($"{requestDTO.ApiType}, {requestDTO.Url}: Response received successfully.");
                             break;
                     }
+
                 }
                 catch (Exception e)
                 {
                     finalResponseDTO.ErrorMessage = new List<string>() { "Error countered:", e.Message.ToString() };
+                    _logger.LogError($"{requestDTO.ApiType}, {requestDTO.Url}: {finalResponseDTO.ErrorMessage}");
                 }
 
                 return (T)(object)finalResponseDTO;
